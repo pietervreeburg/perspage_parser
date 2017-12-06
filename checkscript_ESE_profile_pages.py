e47a4ccdf56667c964cd5df599d3d039319e8d47
Pieter Vreeburg <pietervreeburg@gmail.com>
2017-12-06 15:18:53 +0100

    Done for today, detail scraping mostly works

--- checkscript_ESE_profile_pages.py
+++ checkscript_ESE_profile_pages.py
@@ -1,7 +1,5 @@
 # Checkscript for ESE profile pages
-# Pieter Vreeburg, 6-12-2017
-
-# new version for Drupal profile pages
+# Pieter Vreeburg, 6-12-2017 (New version for Drupal profile pages)
 
 # imports
 import os # from std. library, os interactions
@@ -21,9 +19,6 @@ base_url = 'https://beta.eur.nl/'
 listview_url = 'people?f[0]=researcher_profiles_organisation%3A14&page='
 photo_url = 'typo3temp/pics/'
 
-# eraskop = 'https://beta.eur.nl/modules/custom/eur_people_pages/img/profile-default-image.jpg'
-# foto alfons = 'https://beta.eur.nl/sites/corporate/files/styles/profile/public/externals/972540dbcc07cdb662a12ce14eb9df91.jpg?itok=ziQlgrwd'
-
 # functions
 def write_report(data, report_name):
     data = sorted(data)
@@ -36,13 +31,12 @@ def write_json_file(dict, file_name):
     outfile = open(os.path.join(main_dir, report_dir, '{}_{}.json'.format(file_name, datetime.date.today())), 'w')
     json.dump(dict, outfile, indent = 4)
 
-detail_url_list = []
+detail_page_url_list = []
 char = 20
 # build profile_datastore dictionary
 # use listview to build profile_datastore and get detail_page_url
 while True:
     request_url = '{}{}{}'.format(base_url, listview_url, char)
-    print request_url
     listview_page = requests.get(request_url).text
     sleep(1)
     listview_page_soup = bs4.BeautifulSoup(listview_page, 'lxml')
@@ -53,86 +47,75 @@ while True:
     for profile_div in profile_divs:
         name_link = profile_div.find('a')
         detail_page_url = name_link['href']
-        detail_url_list.append(detail_page_url)
+        detail_page_url_list.append(detail_page_url)
     char += 1
 
-quit() # debug
-
-# detail pages
-
-# photo_div = profile.find('div', class_ = 'headshot')
-# links = listing_div.find_all('a')
-# email = links[1]['href'].split(':')[-1]
-# func = listing_div.find('h5').string
-# photo_url = photo_div.find('img')['src'] # typo3temp/pics/2f243ef8bd.jpg
-
-# profile_datastore = {}
-# profile_datastore[email] = {'name' : name,
-                        # 'func' : None,
-                        # 'photo_url' : None,
-                        # 'detail_page_url' : detail_page_url,
-                        # 'has_detail_page' : None,
-                        # 'research_progs' : None,
-                        # 'cv' : None,
-                        # 'linked_in' : None,
-                        # 'room_nr': None,
-                        # 'tel_nr' : None,
-                        # 'story' : None}
-
-
-for email, profile in profile_datastore.items():
-    print 'Downloading: ', profile['name']
+# use detail pages to fill profile_datastore
+profile_datastore = {}
+for detail_page_url in detail_page_url_list:
+    print 'Downloading:', detail_page_url
     sleep(1)
-    detail_page = requests.get(profile['detail_page_url']).text
-    # missing_detail_page
-    if 'In contact with experts' in detail_page:
-        profile_datastore[email]['has_detail_page'] = False # some people are included in the listview, but have no detail page
-        continue
-    else:
-        profile_datastore[email]['has_detail_page'] = True # some people are included in the listview, but have no detail page
+    detail_page_url = '{}{}'.format(base_url, detail_page_url)
+    detail_page = requests.get(detail_page_url).text
+    # missing_detail_page, add this later
+    # if 'In contact with experts' in detail_page:
+        # profile_datastore[email]['has_detail_page'] = False # some people are included in the listview, but have no detail page
+        # continue
+    # else:
+        # profile_datastore[email]['has_detail_page'] = True # some people are included in the listview, but have no detail page
     detail_page_soup = bs4.BeautifulSoup(detail_page, 'lxml')
-    # story
-    story_div = detail_page_soup.find('div', class_ = 'panel-body') # Eerste div met class panel-body voor story & cv
+    # info-block, contains: photo, func, room, tel, e-mail
+    info_block = detail_page_soup.find('div', class_ = 'person__info-block background--dark l-column-left')
+    email = info_block.find('a').string
+    profile_datastore[email] = {'name' : None,
+                            'func' : None,
+                            'photo_url' : None,
+                            'detail_page_url' : detail_page_url,
+                            'has_detail_page' : None,
+                            'cv' : None,
+                            'linked_in' : None,
+                            'room_nr': None,
+                            'tel_nr' : None,
+                            'story' : None}
+    # photo
+    photo_url = info_block.find('img')['src']
+    if photo_url:
+        profile_datastore[email]['photo_url'] = photo_url
+    # room number
+    room_nr = info_block.find('dt', string = 'Room').find_next_sibling('dd').string
+    if room_nr:
+        profile_datastore[email]['room_nr'] = room_nr
+    # tel number
+    tel_nr = info_block.find('dt', string = 'Telephone').find_next_sibling('dd').string
+    if tel_nr:
+        profile_datastore[email]['tel_nr'] = tel_nr
+    # name
+    name = detail_page_soup.find('span', class_ = 'person__fullname').string
+    if name:
+        profile_datastore[email]['name'] = name
+    # story, NOG EVEN OVER NADENKEN
+    story_div = detail_page_soup.find('div', class_ = 'person__description')
     story = []
-    for item in story_div:
+    # EXAMPLE    
+    # <div class="person__description"><p><p>Ran Xing is affiliated to the Finance Group of Erasmus School of Economics (Erasmus University Rotterdam). He got his Ph.D from Tilburg University, and he has been a visiting scholar at The Wharton School.</p><p>Ran conducts theoretical and empirical research in asset pricing. His current work focuses on the skill of mutual funds and the pricing of liquidity risk.</p><p>You can find more information on his personal website: <a href="https://sites.google.com/view/xingran">https://sites.google.com/view/xingran</a></p><p> </p><p> </p></p></div>
+    for item in story_div.descendants:
         if isinstance(item, bs4.element.Tag):
-            if item.name == 'p' and len(item.contents) > 0:
+            if len(item.contents) > 0:
                 story.append(unicode(item))
     if len(story) > 0:
         story = ''.join(story).encode('utf-8')
         profile_datastore[email]['story'] = story
     # cv
-    cv_div = detail_page_soup.find('div', class_ = 'alsoseerow')
-    if cv_div:
-        profile_datastore[email]['cv'] = True
-    # research_programmes (can be 0, 1 or more)
-    research_progs_div = detail_page_soup.find('div', id = 'research')
-    if research_progs_div:
-        research_progs_div = research_progs_div.descendants
-        research_progs = []
-        for item in research_progs_div:
-            if isinstance(item, bs4.element.Tag):
-                if item.name == 'h2' and len(item.contents) > 0:
-                    research_progs.append(unicode(item.string))
-        if len(research_progs) > 0:
-            research_progs = ', '.join(research_progs).encode('utf-8')
-            profile_datastore[email]['research_progs'] = research_progs
+    # cv_div = detail_page_soup.find('div', class_ = 'alsoseerow')
+    # if cv_div:
+        # profile_datastore[email]['cv'] = True
     # LinkedIn
-    linked_in = detail_page_soup.find('a', title = 'LinkedIN')
-    if linked_in:
-        linked_in = linked_in['href']
-        profile_datastore[email]['linked_in'] = linked_in
-    # room number & tel number
-    detail_profile_div = detail_page_soup.find('div', id = 'people-profile').strings
-    for item in detail_profile_div:
-        if item.startswith('Room'):
-            item = item.split(':')[-1].strip()
-            if not item == '-':
-                profile_datastore[email]['room_nr'] = item
-        if item.startswith('+31'):
-            profile_datastore[email]['tel_nr'] = item
+    # linked_in = detail_page_soup.find('a', title = 'LinkedIN')['href']
+    # if linked_in:
+        # profile_datastore[email]['linked_in'] = linked_in
 
-write_json_file(profile_datastore, 'profile_datastore_dump')
+    write_json_file(profile_datastore, 'profile_datastore_dump') # debug
+    quit() # debug
 
 # build reports
 remove_page = []
@@ -161,17 +144,14 @@ for item in staff_data:
         continue
     std_output = '{}, {}, {}, {}'.format(dept, profile['name'], email, profile['detail_page_url'])
     # missing_photo & has_photo
-    if profile['photo_url'].split('/')[-1] == '0424775dd8.jpg':
+    if profile['photo_url'].split('/')[-1] == 'profile-default-image.jpg':
         missing_photo.append(std_output)
     else:
         has_photo.append((profile['name'], '{}{}'.format(base_url, profile['photo_url'])))
     # missing_detail_page
-    if profile['has_detail_page'] == False:
-        missing_detail_page.append(std_output)
-        continue
-    # has_research_programmes
-    if profile['research_progs']:
-        has_research_programmes.append(std_output)
+    # if profile['has_detail_page'] == False:
+        # missing_detail_page.append(std_output)
+        # continue
     # has_irregular_staff
     if profile['func'] == 'Irregular Staff':
        has_irregular_staff.append(std_output)
@@ -198,14 +178,14 @@ for email in profile_datastore.keys():
 write_report(remove_page, '1_remove_page_ESE_profile_pages')
 write_report(missing_page, '2_missing_page_ESE_profile_pages')
 write_report(missing_detail_page, '3_missing_detail_page_ESE_profile_pages')
+# 5_has_photo_ESE_profile_pages: see below
 write_report(missing_photo, '4_missing_photos_ESE_profile_pages')
-write_report(has_research_programmes, '6_has_research_programme_ESE_profile_pages')
-write_report(has_irregular_staff, '7_has_irregular_staff_ESE_profile_pages')
-write_report(missing_cv, '8_missing_cv_ESE_profile_pages')
-write_report(missing_cv, '9_missing_LinkedIn_ESE_profile_pages')
-write_report(missing_room_tel, '10_missing_room_tel_ESE_profile_pages')
-write_report(missing_story, '11_missing_story_ESE_profile_pages')
-write_report(has_story, '12_has_story_ESE_profile_pages')
+write_report(has_irregular_staff, '6_has_irregular_staff_ESE_profile_pages')
+write_report(missing_cv, '7_missing_cv_ESE_profile_pages')
+write_report(missing_cv, '8_missing_LinkedIn_ESE_profile_pages')
+write_report(missing_room_tel, '9_missing_room_tel_ESE_profile_pages')
+write_report(missing_story, '10_missing_story_ESE_profile_pages')
+write_report(has_story, '11_has_story_ESE_profile_pages')
 
 # write report 5_has_photo_ESE_profile_pages
 cnt = 1
